55ddcf4f75c1e64c7a79b865d1a44b2ec35174a0
vikle012 <viktor.leek@liu.se>
2022-02-07 13:48:56 +0100

    very minor fixes

--- examples/simulation/van_der_pol_ode.m
+++ examples/simulation/van_der_pol_ode.m
@@ -5,8 +5,8 @@ yops Times: t t0 tf State: x size: [2,1]
 sim = yop.ivp(t0==0, tf==20);
 sim.add(der(x) == [x(2); (1-x(1)^2)*x(2)-x(1)]);
 sim.add(x(t0)  == [2; 0]);
-% res = sim.solve('solver', 'idas', 'points', 200);
-sol = sim.solve('solver', 'ode15s');
+sol = sim.solve('solver', 'idas', 'points', 200);
+% sol = sim.solve('solver', 'ode15s');
 
 figure(1); hold on;
 sol.plot(t, x(1));
